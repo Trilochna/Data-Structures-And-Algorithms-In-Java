496b75bed31159041f7154f9bf11e7443b242a93
TRILOCHNA <vaidtrilochna@gmail.com>
2022-09-23 19:32:40 +0530

    Update Find Element In An Array.java

--- Nados/Arrays/Find Element In An Array/Find Element In An Array.java
+++ Nados/Arrays/Find Element In An Array/Find Element In An Array.java
@@ -1,1 +1,29 @@
-..
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+
+    int arr[] = new int[n];
+
+    for(int i = 0; i < arr.length; i++){
+        arr[i] = scn.nextInt();
+    }
+
+    int d = scn.nextInt();
+    int index = -1; 
+    for(int i = 0; i < arr.length; i++){
+        if(arr[i] == d){
+            index = i;
+            break;
+        } 
+    }
+    System.out.println(index);
+ }
+
+}
