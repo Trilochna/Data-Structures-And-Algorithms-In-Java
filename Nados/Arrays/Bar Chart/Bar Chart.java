0138188f2c97b5e971ed6a8f0ab48934603da593
TRILOCHNA <vaidtrilochna@gmail.com>
2022-09-25 18:49:28 +0530

    Create Bar Chart.java

---
+++ Nados/Arrays/Bar Chart/Bar Chart.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+
+    Scanner scn = new Scanner(System.in);
+
+    int n = scn.nextInt();
+    int arr[] = new int[n];
+
+    for(int i = 0; i < arr.length; i++){
+        arr[i] = scn.nextInt();
+    }
+
+    int max = arr[0];
+    for(int i = 1; i < arr.length; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+
+    for(int floor = max; floor > 0; floor--){
+        for(int i = 0; i < arr.length; i++){
+        if(arr[i] >= floor){
+            System.out.print("*\t");
+        } else {
+        System.out.print("\t");
+        }
+        }
+    System.out.println();
+    }
+ }
+}
