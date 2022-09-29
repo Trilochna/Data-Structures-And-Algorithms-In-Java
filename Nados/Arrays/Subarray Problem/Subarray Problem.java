9b1d10b330e54ea3e98492a55b8d10cf3e767450
TRILOCHNA <vaidtrilochna@gmail.com>
2022-09-29 09:42:54 +0530

    Update Subarray Problem.java

--- Nados/Arrays/Subarray Problem/Subarray Problem.java
+++ Nados/Arrays/Subarray Problem/Subarray Problem.java
@@ -1,1 +1,28 @@
+import java.io.*;
+import java.util.*;
 
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+
+    int[] arr = new int[n];
+    for(int i = 0; i < arr.length; i++){
+        arr[i] = scn.nextInt();
+    }
+
+    for(int i = 0; i < arr.length; i++){
+        for(int j = i; j < arr.length; j++){
+            for(int k = i; k <= j; k++){
+                System.out.print(arr[k] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+ }
+
+}
