cc19572592e1afbb416c1996e3a3e5a802bd45c9
TRILOCHNA <vaidtrilochna@gmail.com>
2023-01-14 17:02:01 +0530

    Create Search In A Sorted 2d Array.java

---
+++ Nados/2D Arrays/Search In A Sorted 2d Array/Search In A Sorted 2d Array.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int[][] arr = new int[n][n];
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        
+        int x = scn.nextInt();
+        
+        int i = 0;
+        int j = arr[0].length - 1;
+        
+        while(i < arr.length && j >= 0){
+            if(x == arr[i][j]){
+                System.out.println(i);
+                System.out.println(j);
+                return;
+            } else if(x < arr[i][j]){
+                j--;
+            } else {
+                i++;
+            }
+        }
+        
+        System.out.println("Not Found");
+    }
+}
