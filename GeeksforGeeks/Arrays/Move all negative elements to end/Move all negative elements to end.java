b4577f03a91047176de8c7f857fb24b4e98552bd
TRILOCHNA <vaidtrilochna@gmail.com>
2023-01-18 22:16:01 +0530

    Create Move all negative elements to end.java

---
+++ GeeksforGeeks/Arrays/Move all negative elements to end/Move all negative elements to end.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            obj.segregateElements(a, n);
+            
+            for(int i=0;i<n;i++)
+            System.out.print(a[i]+" ");
+            
+            System.out.println();
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    public void segregateElements(int arr[], int n)
+    {
+        // Your code goes here
+        int[] temp = new int[n];
+        
+        int j = 0;
+        
+        for(int i = 0; i < n; i++){
+            if(arr[i] >= 0){
+                temp[j++] = arr[i];
+            }
+        }
+        
+            if(j == 0 || j == n){
+               return;
+            }
+        
+        for(int i = 0; i < n; i++){
+            if(arr[i] < 0){
+                temp[j++] = arr[i];
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+                arr[i] = temp[i];
+        }
+        
+    }
+}
