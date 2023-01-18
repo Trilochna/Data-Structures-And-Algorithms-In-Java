0d2ad02ac4c632731e897a62e59cdb65feb235d7
TRILOCHNA <vaidtrilochna@gmail.com>
2023-01-18 13:13:21 +0530

    Update Sort an array of 0s, 1s and 2s.java

--- GeeksforGeeks/Arrays/Sort an array of 0s, 1s and 2s/Sort an array of 0s, 1s and 2s.java
+++ GeeksforGeeks/Arrays/Sort an array of 0s, 1s and 2s/Sort an array of 0s, 1s and 2s.java
@@ -1,1 +1,73 @@
+//{ Driver Code Starts
+//Initial template for Java
 
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function template for Java
+
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        // code here 
+        
+        int low = 0;
+        int high = a.length - 1;
+        int mid = 0, temp = 0;
+        
+        while(mid <= high){
+            switch(a[mid]){
+                case 0:{
+                    temp = a[low];
+                    a[low] = a[mid];
+                    a[mid] = temp;
+                    low++;
+                    mid++;
+                    break;
+                }
+                case 1:{
+                    mid++;
+                    break;
+                }
+                case 2:{
+                    temp = a[high];
+                    a[high] = a[mid];
+                    a[mid] = temp;
+                    high--;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+        while(t-->0){
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for(int i=0; i<n; i++){
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            Solution ob=new Solution();
+            ob.sort012(arr, n);
+            StringBuffer str = new StringBuffer();
+            for(int i=0; i<n; i++){
+                str.append(arr[i]+" ");
+            }
+            System.out.println(str);
+        }
+    }
+}
+
+
+// } Driver Code Ends
