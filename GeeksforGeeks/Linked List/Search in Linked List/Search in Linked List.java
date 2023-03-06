9e5ff887a2e5a31a4106ad9cb5d74f8aef3b7f3c
TRILOCHNA <vaidtrilochna@gmail.com>
2023-03-06 16:52:30 +0530

    Create Search in Linked List.java

---
+++ GeeksforGeeks/Linked List/Search in Linked List/Search in Linked List.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+
+} class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for (int i = 0; i < n - 1; i++) {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            int key = sc.nextInt();
+            Solution g = new Solution();
+            boolean ans = g.searchKey(n, head, key);
+            System.out.println(ans ? "1" : "0");
+            // printList(head);
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/* Node of a linked list
+  class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+class Solution {
+    static boolean searchKey(int n, Node head, int key) {
+        // Code here
+        
+        if(head == null){
+            return false;
+        }
+        
+        if(head.data == key){
+            return true;
+        }
+        
+        Node temp = head;
+        
+        for(int i = 0; i < n; i++){
+            if(temp.data == key){
+                return true;
+            } 
+            temp = temp.next;
+        }
+        return false;
+    }
+}
