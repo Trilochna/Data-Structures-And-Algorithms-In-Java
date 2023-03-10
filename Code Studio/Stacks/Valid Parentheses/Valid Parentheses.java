87062ab4e74a585ee38c53aa76db4fb72b1b18fd
TRILOCHNA <vaidtrilochna@gmail.com>
2023-03-10 23:08:29 +0530

    Update Valid Parentheses.java

--- Code Studio/Stacks/Valid Parentheses/Valid Parentheses.java
+++ Code Studio/Stacks/Valid Parentheses/Valid Parentheses.java
@@ -1,1 +1,24 @@
+import java.util.*;
 
+public class Solution {
+    public static boolean isValidParenthesis(String expression) {
+        // Write your code here.
+
+        Stack<Character> s = new Stack<>();
+
+        for(char c : expression.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                s.add(c);
+            } else {
+                if(s.isEmpty()){
+                    return false;
+                }
+                if(c == ')' && s.peek() != '(' || c == '}' && s.peek() != '{' || c == ']' && s.peek() != '['){
+                    return false;
+                }
+                s.pop();             
+            }
+        }
+        return s.isEmpty();
+    }
+}
