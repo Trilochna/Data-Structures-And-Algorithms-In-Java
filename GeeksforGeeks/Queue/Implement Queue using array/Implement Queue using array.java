c6d7a609770a2351d2faee84ffdcd304ca041cf4
TRILOCHNA <vaidtrilochna@gmail.com>
2023-04-05 19:28:50 +0530

    Create Implement Queue using array.java

---
+++ GeeksforGeeks/Queue/Implement Queue using array/Implement Queue using array.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+
+class GfG
+{
+	public static void main(String args[])
+	{
+		 Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+		 {
+			MyQueue obj = new MyQueue();
+			int Q = sc.nextInt();
+			while(Q>0)
+			{
+				int QueryType = 0;
+				QueryType = sc.nextInt();
+				if(QueryType == 1)
+				{
+					int a = sc.nextInt();
+					
+					obj.push(a);
+					
+				}else if(QueryType == 2)
+				{
+				System.out.print(obj.pop()+" ");
+				}
+				Q--;
+			}
+			System.out.println("");
+			t--;
+		 }
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+
+
+class MyQueue {
+
+    int front, rear;
+	int arr[] = new int[100005];
+
+    MyQueue()
+	{
+		front=0;
+		rear=0;
+	}
+	
+	//Function to push an element x in a queue.
+	void push(int x)
+	{
+	    // Your code here
+	    
+	    arr[rear] = x;
+	    rear++;
+	} 
+
+    //Function to pop an element from queue and return that element.
+	int pop()
+	{
+		// Your code here
+		int x;
+		
+		if(rear == front){
+		    return -1;
+		} else {
+		    x = arr[front];
+		    front++;
+		}
+		
+		return x;
+	} 
+}
+
+
+
+
