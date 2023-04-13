52c237a1e369c23c1ad1ea5b8019a1d2fac2cd13
TRILOCHNA <vaidtrilochna@gmail.com>
2023-04-13 22:01:48 +0530

    Update Circular Queue.java

--- Code Studio/Queues/Circular Queue/Circular Queue.java
+++ Code Studio/Queues/Circular Queue/Circular Queue.java
@@ -1,1 +1,71 @@
+public class CircularQueue 
+{
+	// Initialize your data structure.
+    int front;
+    int rear;
+    int[] arr;
+    int size;
+    
+	public CircularQueue(int n) 
+    {
+	    front=-1;
+        rear=-1;
+        arr=new int[n];
+        size=n;
+	}
 
+	/*
+	   Enqueues 'X' into the queue. Returns true if it gets pushed into the stack,
+	   and false otherwise.
+	*/
+	public boolean enqueue(int value) {
+		// Write your code here.
+        if((front==0 && rear==size-1) || (rear==(front-1)%(size-1)))
+        {
+            return false;
+        }
+        else if(front==-1)
+        {
+            front=0;
+            rear=0;
+        }
+        else if(rear==size-1 & front!=0)
+        {
+            rear=0;
+        }
+        else
+        {
+            rear++;
+        }
+        arr[rear]=value;
+        return true;
+	}
+
+	/*
+	  Dequeues top element from queue. Returns -1 if the stack is empty, otherwise
+	  returns the popped element.
+	*/
+	public int dequeue() 
+    {
+		// Write you code here.
+        if(front==-1)
+        {
+            return -1;
+        }
+        int ans=arr[front];
+        arr[front]=-1;
+        if(front==rear)
+        {
+            front=rear=-1;
+        }
+        else if(front==size-1)
+        {
+            front=0;
+        }
+        else
+        {
+            front++;
+        }
+        return ans;
+	}
+};
