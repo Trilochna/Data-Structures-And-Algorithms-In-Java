92ba706c69a2217d84f6d8e3d3495d0328a8312f
TRILOCHNA <vaidtrilochna@gmail.com>
2023-04-14 22:31:19 +0530

    Update SimplestFactorial.java

--- Recursion/SimplestFactorial/SimplestFactorial.java
+++ Recursion/SimplestFactorial/SimplestFactorial.java
@@ -8,10 +8,12 @@ class Factorial {
             return 1;
         }
         
-        int choti_problem = factorial(n-1);
-        int badi_problem = n*factorial(n-1);
+        return n*factorial(n-1);
         
-        return badi_problem;
+//        int choti_problem = factorial(n-1);
+//        int badi_problem = n*factorial(n-1);
+        
+//        return badi_problem;
     }
     
     public static void main(String[] args) {
