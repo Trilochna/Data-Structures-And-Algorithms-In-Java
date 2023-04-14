46ccda1b8c5ac7f6ea0c2b4c80972417b5ac5687
TRILOCHNA <vaidtrilochna@gmail.com>
2023-04-14 19:50:03 +0530

    Create SimplestFactorial.java

---
+++ Recursion/SimplestFactorial/SimplestFactorial.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Factorial {
+    
+    static int factorial(int n){
+        
+        if(n==0){
+            return 1;
+        }
+        
+        int choti_problem = factorial(n-1);
+        int badi_problem = n*factorial(n-1);
+        
+        return badi_problem;
+    }
+    
+    public static void main(String[] args) {
+        
+        Scanner scn = new Scanner(System.in);
+        
+        int n = scn.nextInt();
+        System.out.println("NUMBER IS : " + n);
+
+        int ans = factorial(n);
+        
+        System.out.println("FACTORIAL IS : " + ans);
+    }
+}
