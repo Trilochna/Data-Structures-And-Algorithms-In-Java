197e96c9f6ed0ef64237a8f7dad19871ff147e81
TRILOCHNA <vaidtrilochna@gmail.com>
2023-04-14 22:59:46 +0530

    Update PowerOf2.java

--- Recursion/PowerOf2/PowerOf2.java
+++ Recursion/PowerOf2/PowerOf2.java
@@ -1,1 +1,28 @@
+import java.util.*;
 
+class PowerOf2 {
+    
+    static int PowerOfTwo(int n){
+        
+        if(n==0){
+            return 1;
+        }
+        
+        int small_problem = PowerOfTwo(n-1);
+        int big_problem = 2*PowerOfTwo(n-1);
+        
+        return big_problem;
+    }
+    
+    public static void main(String[] args) {
+        
+        Scanner scn = new Scanner(System.in);
+        
+        int n = scn.nextInt();
+        System.out.println("Two to the Power : " + n);
+
+        int ans = PowerOfTwo(n);
+        
+        System.out.println("After Power Computation : " + ans);
+    }
+}
