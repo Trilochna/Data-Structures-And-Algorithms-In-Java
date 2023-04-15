375fa266a0cb9ed098709534025298de11973775
TRILOCHNA <vaidtrilochna@gmail.com>
2023-04-15 17:11:06 +0530

    Update 509. Fibonacci Number.java

--- Leetcode/Recursion/509. Fibonacci Number/509. Fibonacci Number.java
+++ Leetcode/Recursion/509. Fibonacci Number/509. Fibonacci Number.java
@@ -1,1 +1,11 @@
-
+class Solution {
+    public int fib(int n) {
+        // base case
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+        // recursive call
+        return fib(n-1)+fib(n-2);
+    }
+}
