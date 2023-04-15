4e379c86fb28f0c967b0673c58967d17aca1de2a
TRILOCHNA <vaidtrilochna@gmail.com>
2023-04-15 14:45:46 +0530

    Update PrintSimpleCounting.java

--- Recursion/PrintSimpleCounting/PrintSimpleCounting.java
+++ Recursion/PrintSimpleCounting/PrintSimpleCounting.java
@@ -1,1 +1,29 @@
+import java.util.*;
 
+class PrintCounting{
+    
+    static void PrintCount(int n){
+        
+        // base case 
+        
+        if(n > 0){
+            
+            // recursive call 
+            
+           PrintCount(n-1);
+           System.out.println(n);
+        } 
+        
+        return;
+    }
+    
+    public static void main(String[] args){
+        
+        Scanner scn = new Scanner(System.in);
+        
+        int n = scn.nextInt();
+        
+        PrintCount(n);
+        
+    }
+}
