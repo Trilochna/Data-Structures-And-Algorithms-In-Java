6c0078347f162d2611d8d9c664df34cab6171e62
TRILOCHNA <vaidtrilochna@gmail.com>
2023-04-16 21:31:45 +0530

    Update Sum Of All Elements In Array.java

--- Recursion/Sum Of All Elements In Array/Sum Of All Elements In Array.java
+++ Recursion/Sum Of All Elements In Array/Sum Of All Elements In Array.java
@@ -1,1 +1,25 @@
-
+public class Main
+{
+    
+    static int arrSum(int[] arr, int size){
+        
+        // base case:
+        
+        if(size <= 0){
+            return 0;
+        }
+        
+        // recursive relation
+        
+        return arrSum(arr, size - 1) + arr[size - 1];
+    }
+    
+	public static void main(String[] args) {
+	    
+	    int size = 5;
+	    int[] arr = {34, 45, 23, 67, 87};
+	    
+	    
+		System.out.println(arrSum(arr, size));
+	}
+}
