7d854bdf87b5e108c5845db8d8e0710962fde4b6
TRILOCHNA <vaidtrilochna@gmail.com>
2023-07-23 19:00:26 +0530

    Update heapifyalgo.java

--- Heaps/heapifyalgo/heapifyalgo.java
+++ Heaps/heapifyalgo/heapifyalgo.java
@@ -114,7 +114,7 @@ public class Main
 	    // to implement heapify function
 	    int arr[] = new int[] { -1, 54, 53, 55, 52, 50 };
 	    int n = 5;
-	    for(int i = n/2; i >= 0; i--){
+	    for(int i = n/2; i > 0; i--){
 	        heapify(arr, n, i);
 	    }
 	    System.out.println("Print the array now :");
