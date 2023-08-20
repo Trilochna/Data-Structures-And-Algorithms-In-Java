2c41b31a9367f4100942a7f4a294c216fb18c0d0
TRILOCHNA <vaidtrilochna@gmail.com>
2023-08-20 17:29:18 +0530

    Create 145. Binary Tree Postorder Traversal.java

---
+++ Leetcode/145. Binary Tree Postorder Traversal.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        helper(root, result);
+        return result;
+    }
+    public void helper(TreeNode root, List<Integer> result){
+        if(root == null){ return; }
+
+        helper(root.left, result);
+        helper(root.right, result);
+        result.add(root.val);
+    }
+}
