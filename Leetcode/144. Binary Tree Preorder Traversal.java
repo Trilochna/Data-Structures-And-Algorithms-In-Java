b6c1f8cbdd045f26f44e886f428d1f9301f0c38f
TRILOCHNA <vaidtrilochna@gmail.com>
2023-08-20 17:27:44 +0530

    Create 144. Binary Tree Preorder Traversal.java

---
+++ Leetcode/144. Binary Tree Preorder Traversal.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        helper(root, result);
+        return result;
+    }
+
+    public void helper(TreeNode root, List<Integer> result){
+        if(root == null){ return; }
+
+        result.add(root.val);
+        helper(root.left, result);
+        helper(root.right, result);
+    }
+}
